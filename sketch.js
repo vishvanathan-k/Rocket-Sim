4f895aa1dab2f08209e4eeb5a8045ca1c3d6e3fc
Vishvanathan K <vishvanathank@outlook.in>
2021-10-10 15:18:09 +0530

    improve the simulation

--- sketch.js
+++ sketch.js
@@ -6,7 +6,6 @@ let fuel = 850
 let fuelconsum = 0.1
 let v = 0
 let s = 0
-let prevS = 0
 
 function setup() {
     createCanvas(500, 1000)
@@ -17,12 +16,11 @@ function draw() {
     mass = 150+fuel
     Rocket(X,Y)
     a = acc(mass,maxthrust,efficiency)
-    console.log(a)
     s = v + 0.5*a
     v = v + a
-    Y -= s - prevS
-    fuel -= fuelconsum
-    prevS = s
+    console.log(a+" "+s);
+    Y -= s*0.01
+    fuel -= fuel > 0 ? fuelconsum : 0
 }
 
 function Rocket(X,Y){
